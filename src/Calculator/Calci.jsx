b5d5d9d25135a97031999ef1693ff947b6bfc713
Shalinich12 <shalinichellingi9@gmail.com>
2025-04-04 22:13:32 +0530

    calculator

--- src/Calculator/Calci.jsx
+++ src/Calculator/Calci.jsx
@@ -1,34 +1,40 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import './cacli.css'
 function Calci() {
     const [input, setNum1] = useState('');
-    const handleButtonClick = (event)=>{
-        const value=event.target.value;
-        if(value==="="){
-            console.log(eval(value));
+    const handleButtonClick = (event) => {
+        const value = event.target.value;
+        if (value === "=") {
+            try {
+                setNum1(eval(input).toString());
+            } catch {
+                setNum1("Error");
+            }
+        } else if (value === 'C') {
+            setNum1("");
+        } else {
+            setNum1(input + value);
         }
-
     }
-  return (
-    <div className="Calculator-Container">
-        <div className="Calculator">
-            <input className="Calculator-display" type="text" value={input} />
-            <div className="calculator-buttons">
-                {[7,8,9,'*',
-                    4,5,6,'-',
-                    1,2,3,'+',
-                    0,'.','=',
-                ]
-                .map((btn, i) => (
-                    <button key={i} value={btn} onClick={handleButtonClick}
-                    >{btn}</button>
-                    ))}
-                    <button className='clear' value="C" onClick={handleButtonClick}>C</button>                
+    return (
+        <div className="Calculator-Container">
+            <div className="Calculator">
+                <input className="calculator-display" type="text" value={input} readOnly/>
+                <div className="calculator-buttons">
+                    {[7, 8, 9, '*',
+                        4, 5, 6, '-',
+                        1, 2, 3, '+',
+                        0, '.', '=', '%',
+                    ]
+                        .map((btn, i) => (
+                            <button key={i} value={btn} onClick={handleButtonClick}
+                            >{btn}</button>
+                        ))}
+                    <button className='clear' value="C" onClick={handleButtonClick}>C</button>
+                </div>
             </div>
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Calci
